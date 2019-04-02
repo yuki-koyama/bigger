7b91ed6bfec7acc06eb67625ca8fe1eb5efac8f3
Yuki Koyama <yuki@koyama.xyz>
2019-04-02 19:17:37 +0900

    Add a utility function to obtain shader paths

--- include/bigger/bigger.hpp
+++ include/bigger/bigger.hpp
@@ -1,6 +1,12 @@
 #ifndef bigger_hpp
 #define bigger_hpp
 
+#include <string>
 #include <bigg.hpp>
+
+namespace bigger
+{
+    std::string getShaderDirectoryPath(const bgfx::RendererType::Enum renderer_type);
+}
 
 #endif // bigger_hpp
---
+++ src/bigger.cpp
@@ -0,0 +1,23 @@
+#include <bigger/bigger.hpp>
+#include <stdexcept>
+
+namespace bigger
+{
+    std::string getShaderDirectoryPath(const bgfx::RendererType::Enum renderer_type)
+    {
+        switch (renderer_type)
+        {
+            case bgfx::RendererType::Noop:                               break;
+            case bgfx::RendererType::Direct3D9:  return "shaders/dx9";   break;
+            case bgfx::RendererType::Direct3D11:
+            case bgfx::RendererType::Direct3D12: return "shaders/dx11";  break;
+            case bgfx::RendererType::Gnm:                                break;
+            case bgfx::RendererType::Metal:      return "shaders/metal"; break;
+            case bgfx::RendererType::OpenGL:     return "shaders/glsl";  break;
+            case bgfx::RendererType::OpenGLES:   return "shaders/essl";  break;
+            case bgfx::RendererType::Vulkan:                             break;
+            case bgfx::RendererType::Count:                              break;
+        }
+        throw std::runtime_error("Renderer type not supported.");
+    }
+}
