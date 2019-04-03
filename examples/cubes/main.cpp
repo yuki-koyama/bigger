6c1e9cb232e1aa912ef876cd6061a77b2e7e3027
Yuki Koyama <yuki@koyama.xyz>
2019-04-03 09:52:23 +0900

    Favor less nested coding style

--- examples/cubes/main.cpp
+++ examples/cubes/main.cpp
@@ -8,76 +8,15 @@ class CubeApp : public bigg::Application
 {
 public:
 
-    CubeApp() :
-    m_camera(
-    {
-        { 0.0, 0.0, - 5.0 },
-        { 0.0, 0.0, 0.0 },
-        { 0.0, 1.0, 0.0 }
-    })
-    {
-    }
-
-    void initialize(int argc, char** argv) override
-    {
-        const std::string shader_dir_path = bigger::getShaderDirectoryPath(bgfx::getRendererType());
-        const std::string vs_path = shader_dir_path + "/" + "vs_blinnphong.bin";
-        const std::string fs_path = shader_dir_path + "/" + "fs_blinnphong.bin";
-
-        m_program = bigg::loadProgram(vs_path.c_str(), fs_path.c_str());
-
-        m_cube.initializePrimitive();
-    }
-
-    void onReset() override
-    {
-        constexpr uint32_t bg_color = 0x303030ff;
-
-        bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, bg_color, 1.0f, 0);
-        bgfx::setViewRect(0, 0, 0, uint16_t(getWidth()), uint16_t(getHeight()));
-    }
-
-    void update(float dt) override
-    {
-        m_time += dt;
-
-        const glm::mat4 view_matrix = m_camera.getViewMatrix();
-        const glm::mat4 proj_matrix = glm::perspective(glm::radians(60.0f), getAspect(), 0.1f, 100.0f);
-
-        bgfx::setViewTransform(0, glm::value_ptr(view_matrix), glm::value_ptr(proj_matrix));
-        bgfx::setViewRect(0, 0, 0, uint16_t(getWidth()), uint16_t(getHeight()));
-        bgfx::touch(0);
-
-        constexpr int n = 4;
-        for (int x = - n; x <= n; ++ x)
-        {
-            for (int y = - n; y <= n; ++ y)
-            {
-                glm::mat4 model_matrix(1.0f);
-                model_matrix = glm::translate(model_matrix, glm::vec3(x, y, 0.0f));
-                model_matrix = glm::rotate(model_matrix, m_time, glm::vec3(x, y, 1.0f));
-                model_matrix = glm::scale(model_matrix, glm::vec3(2.0f / n));
-                bgfx::setTransform(glm::value_ptr(model_matrix));
-
-                m_cube.submitPrimitive(m_program);
-            }
-        }
-    }
-
-    int shutdown() override
-    {
-        bgfx::destroy(m_program);
-        m_cube.destroyPrimitive();
-
-        return 0;
-    }
+    CubeApp();
+    void initialize(int argc, char** argv) override;
+    void onReset() override;
+    void update(float dt) override;
+    int shutdown() override;
 
 private:
 
-    float getAspect()
-    {
-        return float(getWidth()) / float(getHeight());
-    }
+    float getAspect() { return float(getWidth()) / float(getHeight()); }
 
     bgfx::ProgramHandle m_program;
     bigger::Cube m_cube;
@@ -85,6 +24,67 @@ private:
     bigger::Camera m_camera;
     float m_time;
 };
+
+CubeApp::CubeApp()
+{
+    m_camera.position = { 0.0, 0.0, - 5.0 };
+    m_camera.target   = { 0.0, 0.0, 0.0 };
+    m_camera.up       = { 0.0, 1.0, 0.0 };
+}
+
+void CubeApp::initialize(int argc, char** argv)
+{
+    const std::string shader_dir_path = bigger::getShaderDirectoryPath(bgfx::getRendererType());
+    const std::string vs_path = shader_dir_path + "/" + "vs_blinnphong.bin";
+    const std::string fs_path = shader_dir_path + "/" + "fs_blinnphong.bin";
+
+    m_program = bigg::loadProgram(vs_path.c_str(), fs_path.c_str());
+
+    m_cube.initializePrimitive();
+}
+
+void CubeApp::onReset()
+{
+    constexpr uint32_t bg_color = 0x303030ff;
+
+    bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, bg_color, 1.0f, 0);
+    bgfx::setViewRect(0, 0, 0, uint16_t(getWidth()), uint16_t(getHeight()));
+}
+
+void CubeApp::update(float dt)
+{
+    m_time += dt;
+
+    const glm::mat4 view_matrix = m_camera.getViewMatrix();
+    const glm::mat4 proj_matrix = glm::perspective(glm::radians(60.0f), getAspect(), 0.1f, 100.0f);
+
+    bgfx::setViewTransform(0, glm::value_ptr(view_matrix), glm::value_ptr(proj_matrix));
+    bgfx::setViewRect(0, 0, 0, uint16_t(getWidth()), uint16_t(getHeight()));
+    bgfx::touch(0);
+
+    constexpr int n = 4;
+    for (int x = - n; x <= n; ++ x)
+    {
+        for (int y = - n; y <= n; ++ y)
+        {
+            glm::mat4 model_matrix(1.0f);
+            model_matrix = glm::translate(model_matrix, glm::vec3(x, y, 0.0f));
+            model_matrix = glm::rotate(model_matrix, m_time, glm::vec3(x, y, 1.0f));
+            model_matrix = glm::scale(model_matrix, glm::vec3(2.0f / n));
+            bgfx::setTransform(glm::value_ptr(model_matrix));
+
+            m_cube.submitPrimitive(m_program);
+        }
+    }
+}
+
+int CubeApp::shutdown()
+{
+    bgfx::destroy(m_program);
+    m_cube.destroyPrimitive();
+
+    return 0;
+}
 
 int main(int argc, char** argv)
 {
--- src/bigger.cpp
+++ src/bigger.cpp
@@ -1,23 +1,20 @@
 #include <bigger/bigger.hpp>
 #include <stdexcept>
 
-namespace bigger
+std::string bigger::getShaderDirectoryPath(const bgfx::RendererType::Enum renderer_type)
 {
-    std::string getShaderDirectoryPath(const bgfx::RendererType::Enum renderer_type)
+    switch (renderer_type)
     {
-        switch (renderer_type)
-        {
-            case bgfx::RendererType::Noop:                               break;
-            case bgfx::RendererType::Direct3D9:  return "shaders/dx9";   break;
-            case bgfx::RendererType::Direct3D11:
-            case bgfx::RendererType::Direct3D12: return "shaders/dx11";  break;
-            case bgfx::RendererType::Gnm:                                break;
-            case bgfx::RendererType::Metal:      return "shaders/metal"; break;
-            case bgfx::RendererType::OpenGL:     return "shaders/glsl";  break;
-            case bgfx::RendererType::OpenGLES:   return "shaders/essl";  break;
-            case bgfx::RendererType::Vulkan:                             break;
-            case bgfx::RendererType::Count:                              break;
-        }
-        throw std::runtime_error("Renderer type not supported.");
+        case bgfx::RendererType::Noop:                               break;
+        case bgfx::RendererType::Direct3D9:  return "shaders/dx9";   break;
+        case bgfx::RendererType::Direct3D11:
+        case bgfx::RendererType::Direct3D12: return "shaders/dx11";  break;
+        case bgfx::RendererType::Gnm:                                break;
+        case bgfx::RendererType::Metal:      return "shaders/metal"; break;
+        case bgfx::RendererType::OpenGL:     return "shaders/glsl";  break;
+        case bgfx::RendererType::OpenGLES:   return "shaders/essl";  break;
+        case bgfx::RendererType::Vulkan:                             break;
+        case bgfx::RendererType::Count:                              break;
     }
+    throw std::runtime_error("Renderer type not supported.");
 }
