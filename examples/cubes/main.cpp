3bcd34287e689edb8b87dc8072428bc0a0b3e789
Yuki Koyama <yuki@koyama.xyz>
2019-04-06 11:06:10 +0900

    Add a utility app class

--- examples/cubes/main.cpp
+++ examples/cubes/main.cpp
@@ -1,14 +1,15 @@
+#include <cmath>
 #include <bigger/bigger.hpp>
-#include <bigger/camera.hpp>
 #include <bigger/primitives/cube.hpp>
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
 
-class CubeApp : public bigg::Application
+class CubeApp : public bigger::Application
 {
 public:
 
     CubeApp();
+
     void initialize(int argc, char** argv) override;
     void onReset() override;
     void update(float dt) override;
@@ -16,20 +17,17 @@ public:
 
 private:
 
-    float getAspect() const { return float(getWidth()) / float(getHeight()); }
-
     bgfx::ProgramHandle m_program;
     bigger::Cube m_cube;
 
-    bigger::Camera m_camera;
     float m_time;
+
+    int m_massive_level;
 };
 
 CubeApp::CubeApp()
 {
-    m_camera.position = { 0.0f, 0.0f, - 5.0f };
-    m_camera.target   = { 0.0f, 0.0f, 0.0f };
-    m_camera.up       = { 0.0f, 1.0f, 0.0f };
+    m_massive_level = 2;
 }
 
 void CubeApp::initialize(int argc, char** argv)
@@ -48,37 +46,32 @@ void CubeApp::onReset()
     constexpr uint32_t bg_color = 0x303030ff;
 
     bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, bg_color, 1.0f, 0);
-    bgfx::setViewRect(0, 0, 0, uint16_t(getWidth()), uint16_t(getHeight()));
 }
 
 void CubeApp::update(float dt)
 {
     m_time += dt;
 
-    const glm::mat4 view_matrix = m_camera.getViewMatrix();
-    const glm::mat4 proj_matrix = glm::perspective(glm::radians(60.0f), getAspect(), 0.1f, 100.0f);
-
-    bgfx::setViewTransform(0, glm::value_ptr(view_matrix), glm::value_ptr(proj_matrix));
-    bgfx::setViewRect(0, 0, 0, uint16_t(getWidth()), uint16_t(getHeight()));
-    bgfx::touch(0);
-
-    static int n = 4;
-
     ImGui::Begin("Config");
     {
-        ImGui::SliderInt("n", &n, 1, 8);
-        ImGui::SliderFloat3("m_camera.position", glm::value_ptr(m_camera.position), - 5.0f, 5.0f);
+        ImGui::SliderInt("massive_level", &m_massive_level, 1, 8);
+        ImGui::SliderFloat3("camera.position", glm::value_ptr(getCamera().position), - 5.0f, 5.0f);
     }
     ImGui::End();
 
-    for (int x = - n; x <= n; ++ x)
+    setViewProj();
+    setRect();
+
+    bgfx::touch(0);
+
+    for (int x = - m_massive_level; x <= m_massive_level; ++ x)
     {
-        for (int y = - n; y <= n; ++ y)
+        for (int y = - m_massive_level; y <= m_massive_level; ++ y)
         {
             glm::mat4 model_matrix(1.0f);
             model_matrix = glm::translate(model_matrix, glm::vec3(x, y, 0.0f));
             model_matrix = glm::rotate(model_matrix, m_time, glm::vec3(x, y, 1.0f));
-            model_matrix = glm::scale(model_matrix, glm::vec3(2.0f / n));
+            model_matrix = glm::scale(model_matrix, glm::vec3(std::pow(3.0f, - 0.5f)));
             bgfx::setTransform(glm::value_ptr(model_matrix));
 
             m_cube.submitPrimitive(m_program);
--- include/bigger/bigger.hpp
+++ include/bigger/bigger.hpp
@@ -3,10 +3,48 @@
 
 #include <string>
 #include <bigg.hpp>
+#include <bigger/camera.hpp>
 
 namespace bigger
 {
     std::string getShaderDirectoryPath(const bgfx::RendererType::Enum renderer_type);
+
+    class Application : public bigg::Application
+    {
+    public:
+
+        Application()
+        {
+            m_camera.position = { 1.0f, 1.0f, - 2.0f };
+            m_camera.target   = { 0.0f, 0.0f, 0.0f };
+            m_camera.up       = { 0.0f, 1.0f, 0.0f };
+        }
+
+        void setRect()
+        {
+            bgfx::setViewRect(0, 0, 0, uint16_t(getWidth()), uint16_t(getHeight()));
+        }
+
+        void setViewProj()
+        {
+            const glm::mat4 view_matrix = m_camera.getViewMatrix();
+            const glm::mat4 proj_matrix = glm::perspective(glm::radians(60.0f), getAspect(), 0.1f, 100.0f);
+
+            bgfx::setViewTransform(0, glm::value_ptr(view_matrix), glm::value_ptr(proj_matrix));
+        }
+
+        float getAspect() const
+        {
+            return float(getWidth()) / float(getHeight());
+        }
+
+        const Camera& getCamera() const { return m_camera; }
+        Camera& getCamera() { return m_camera; }
+
+    private:
+
+        Camera m_camera;
+    };
 }
 
 #endif // bigger_hpp
