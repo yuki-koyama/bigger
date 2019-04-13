e7086aa697715a209562cd9643fcb1aff05ee8b6
Yuki Koyama <yuki@koyama.xyz>
2019-04-14 00:42:02 +0900

    Add a plane primitive class

---
+++ include/bigger/primitives/plane-primitive.hpp
@@ -0,0 +1,31 @@
+#ifndef plane_primitive_hpp
+#define plane_primitive_hpp
+
+#include <bigger/primitive.hpp>
+
+namespace bigger
+{
+    class PlanePrimitive : public Primitive
+    {
+    public:
+
+        PlanePrimitive()
+        {
+            m_vertices =
+            {
+                { { +0.5f, +0.0f, -0.5f }, { 0.0f, +1.0f, 0.0f } },
+                { { -0.5f, +0.0f, -0.5f }, { 0.0f, +1.0f, 0.0f } },
+                { { -0.5f, +0.0f, +0.5f }, { 0.0f, +1.0f, 0.0f } },
+                { { +0.5f, +0.0f, +0.5f }, { 0.0f, +1.0f, 0.0f } },
+            };
+
+            m_triangle_list =
+            {
+                0, 1, 2,
+                0, 2, 3,
+            };
+        }
+    };
+}
+
+#endif /* plane_primitive_hpp */
