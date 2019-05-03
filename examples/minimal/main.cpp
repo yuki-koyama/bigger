686651b61f94c9d5cac5d4a886fdaaabfc2da0a2
Yuki Koyama <yuki@koyama.xyz>
2019-05-03 13:30:36 +0900

    Make the minimal example even more minimalistic

--- examples/minimal/main.cpp
+++ examples/minimal/main.cpp
@@ -6,26 +6,10 @@ public:
 
     MinimalApp() {}
 
-    void initialize(int argc, char** argv) override;
-    void updateApp() override;
-    void releaseSharedResources() override;
+    void initialize(int argc, char** argv) override {}
+    void updateApp() override {}
+    void releaseSharedResources() override {}
 };
-
-void MinimalApp::initialize(int argc, char** argv)
-{
-    reset(BGFX_RESET_VSYNC | BGFX_RESET_MSAA_X8);
-    bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x303030ff, 1.0f, 0);
-}
-
-void MinimalApp::updateApp()
-{
-    ImGui::Begin("Hello World!");
-    ImGui::End();
-}
-
-void MinimalApp::releaseSharedResources()
-{
-}
 
 int main(int argc, char** argv)
 {
