a3f9188a9b4777febfd37038003071d5cc17c9a1
Yuki Koyama <yuki@koyama.xyz>
2020-12-27 12:28:45 +0900

    Fix warning

--- src/utils.cpp
+++ src/utils.cpp
@@ -5,30 +5,23 @@ std::string bigger::getShaderDirectoryPath(const bgfx::RendererType::Enum render
 {
     switch (renderer_type)
     {
-        case bgfx::RendererType::Noop:
-            break;
-        case bgfx::RendererType::Direct3D9:
-            return "shaders/dx9";
-            break;
-        case bgfx::RendererType::Direct3D11:
-        case bgfx::RendererType::Direct3D12:
-            return "shaders/dx11";
-            break;
-        case bgfx::RendererType::Gnm:
-            break;
-        case bgfx::RendererType::Metal:
-            return "shaders/metal";
-            break;
-        case bgfx::RendererType::OpenGL:
-            return "shaders/glsl";
-            break;
-        case bgfx::RendererType::OpenGLES:
-            return "shaders/essl";
-            break;
-        case bgfx::RendererType::Vulkan:
-            break;
-        case bgfx::RendererType::Count:
-            break;
+    case bgfx::RendererType::Direct3D9:
+        return "shaders/dx9";
+    case bgfx::RendererType::Direct3D11:
+    case bgfx::RendererType::Direct3D12:
+        return "shaders/dx11";
+    case bgfx::RendererType::Metal:
+        return "shaders/metal";
+    case bgfx::RendererType::OpenGL:
+        return "shaders/glsl";
+    case bgfx::RendererType::OpenGLES:
+        return "shaders/essl";
+    case bgfx::RendererType::Nvn:
+    case bgfx::RendererType::Gnm:
+    case bgfx::RendererType::Noop:
+    case bgfx::RendererType::Vulkan:
+    case bgfx::RendererType::Count:
+        break;
     }
     throw std::runtime_error("Renderer type not supported.");
 }
