57332f29c0724e4c2addee5a0b1e93071d25e326
Yuki Koyama <yuki@koyama.xyz>
2021-01-02 22:55:35 +0900

    Add utility functions to load textures

--- include/bigger/utils.hpp
+++ include/bigger/utils.hpp
@@ -2,11 +2,19 @@
 #define BIGGER_UTILS_HPP
 
 #include <bgfx/bgfx.h>
+#include <bimg/bimg.h>
 #include <string>
 
 namespace bigger
 {
     std::string getShaderDirectoryPath(const bgfx::RendererType::Enum renderer_type);
-}
+
+    // A wrapper function to use utility functions in bgfx's examples
+    bgfx::TextureHandle loadTexture(const char*              name,
+                                    uint64_t                 flags       = BGFX_TEXTURE_NONE | BGFX_SAMPLER_NONE,
+                                    uint8_t                  skip        = 0,
+                                    bgfx::TextureInfo*       info        = NULL,
+                                    bimg::Orientation::Enum* orientation = NULL);
+} // namespace bigger
 
 #endif // BIGGER_UTILS_HPP
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,157 @@
 #include <bigger/utils.hpp>
+#include <bimg/decode.h>
+#include <bx/file.h>
 #include <stdexcept>
+
+#define DBG(_format, ...) printf("Error")
+
+// The following functions in the "bgfx_utils" namespace were originally distributed in the bgfx repository (in the
+// examples directory) under the BSD 2-clause license. Yuki Koyama modified them slightly and redistribute them under
+// the MIT license here.
+namespace bgfx_utils
+{
+    /*
+     * Copyright 2011-2020 Branimir Karadzic. All rights reserved.
+     * License: https://github.com/bkaradzic/bimg#license-bsd-2-clause
+     */
+
+    bx::AllocatorI* getAllocator()
+    {
+        static bx::DefaultAllocator s_allocator;
+        return &s_allocator;
+    }
+
+    bx::FileReaderI* getFileReader()
+    {
+        static bx::FileReader s_file_reader;
+        return &s_file_reader;
+    }
+
+    void* load(bx::FileReaderI* _reader, bx::AllocatorI* _allocator, const char* _filePath, uint32_t* _size)
+    {
+        if (bx::open(_reader, _filePath))
+        {
+            uint32_t size = (uint32_t) bx::getSize(_reader);
+            void*    data = BX_ALLOC(_allocator, size);
+            bx::read(_reader, data, size);
+            bx::close(_reader);
+            if (NULL != _size)
+            {
+                *_size = size;
+            }
+            return data;
+        }
+        else
+        {
+            DBG("Failed to open: %s.", _filePath);
+        }
+
+        if (NULL != _size)
+        {
+            *_size = 0;
+        }
+
+        return NULL;
+    }
+
+    void* load(const char* _filePath, uint32_t* _size)
+    {
+        return load(getFileReader(), getAllocator(), _filePath, _size);
+    }
+
+    void unload(void* _ptr) { BX_FREE(getAllocator(), _ptr); }
+
+    static void imageReleaseCb(void* _ptr, void* _userData)
+    {
+        BX_UNUSED(_ptr);
+        bimg::ImageContainer* imageContainer = (bimg::ImageContainer*) _userData;
+        bimg::imageFree(imageContainer);
+    }
+
+    bgfx::TextureHandle loadTexture(bx::FileReaderI*         _reader,
+                                    const char*              _filePath,
+                                    uint64_t                 _flags,
+                                    uint8_t                  _skip,
+                                    bgfx::TextureInfo*       _info,
+                                    bimg::Orientation::Enum* _orientation)
+    {
+        BX_UNUSED(_skip);
+        bgfx::TextureHandle handle = BGFX_INVALID_HANDLE;
+
+        uint32_t size;
+        void*    data = load(_reader, getAllocator(), _filePath, &size);
+        if (NULL != data)
+        {
+            bimg::ImageContainer* imageContainer = bimg::imageParse(getAllocator(), data, size);
+
+            if (NULL != imageContainer)
+            {
+                if (NULL != _orientation)
+                {
+                    *_orientation = imageContainer->m_orientation;
+                }
+
+                const bgfx::Memory* mem =
+                    bgfx::makeRef(imageContainer->m_data, imageContainer->m_size, imageReleaseCb, imageContainer);
+                unload(data);
+
+                if (imageContainer->m_cubeMap)
+                {
+                    handle = bgfx::createTextureCube(uint16_t(imageContainer->m_width),
+                                                     1 < imageContainer->m_numMips,
+                                                     imageContainer->m_numLayers,
+                                                     bgfx::TextureFormat::Enum(imageContainer->m_format),
+                                                     _flags,
+                                                     mem);
+                }
+                else if (1 < imageContainer->m_depth)
+                {
+                    handle = bgfx::createTexture3D(uint16_t(imageContainer->m_width),
+                                                   uint16_t(imageContainer->m_height),
+                                                   uint16_t(imageContainer->m_depth),
+                                                   1 < imageContainer->m_numMips,
+                                                   bgfx::TextureFormat::Enum(imageContainer->m_format),
+                                                   _flags,
+                                                   mem);
+                }
+                else if (bgfx::isTextureValid(0,
+                                              false,
+                                              imageContainer->m_numLayers,
+                                              bgfx::TextureFormat::Enum(imageContainer->m_format),
+                                              _flags))
+                {
+                    handle = bgfx::createTexture2D(uint16_t(imageContainer->m_width),
+                                                   uint16_t(imageContainer->m_height),
+                                                   1 < imageContainer->m_numMips,
+                                                   imageContainer->m_numLayers,
+                                                   bgfx::TextureFormat::Enum(imageContainer->m_format),
+                                                   _flags,
+                                                   mem);
+                }
+
+                if (bgfx::isValid(handle))
+                {
+                    bgfx::setName(handle, _filePath);
+                }
+
+                if (NULL != _info)
+                {
+                    bgfx::calcTextureSize(*_info,
+                                          uint16_t(imageContainer->m_width),
+                                          uint16_t(imageContainer->m_height),
+                                          uint16_t(imageContainer->m_depth),
+                                          imageContainer->m_cubeMap,
+                                          1 < imageContainer->m_numMips,
+                                          imageContainer->m_numLayers,
+                                          bgfx::TextureFormat::Enum(imageContainer->m_format));
+                }
+            }
+        }
+
+        return handle;
+    }
+
+} // namespace bgfx_utils
 
 std::string bigger::getShaderDirectoryPath(const bgfx::RendererType::Enum renderer_type)
 {
@@ -24,4 +176,10 @@ std::string bigger::getShaderDirectoryPath(const bgfx::RendererType::Enum render
         break;
     }
     throw std::runtime_error("Renderer type not supported.");
+}
+
+bgfx::TextureHandle bigger::loadTexture(
+    const char* name, uint64_t flags, uint8_t skip, bgfx::TextureInfo* info, bimg::Orientation::Enum* orientation)
+{
+    return bgfx_utils::loadTexture(bgfx_utils::getFileReader(), name, flags, skip, info, orientation);
 }
