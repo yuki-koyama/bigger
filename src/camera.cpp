01edc503d639ce4c62d74821c4fec9f2a41950ad
Yuki Koyama <yuki@koyama.xyz>
2021-01-04 22:31:40 +0900

    Improve camera parameter control

--- src/camera.cpp
+++ src/camera.cpp
@@ -4,7 +4,7 @@
 void bigger::Camera::drawImgui()
 {
     ImGui::Text("Camera Setting");
-    ImGui::SliderFloat3("camera.position", glm::value_ptr(m_position), -5.0f, 5.0f);
-    ImGui::SliderFloat3("camera.target", glm::value_ptr(m_target), -2.0f, 2.0f);
-    ImGui::SliderFloat("camera.fov", &m_fov, 10.0f, 120.0f);
+    ImGui::SliderFloat3("Position", glm::value_ptr(m_position), -5.0f, 5.0f);
+    ImGui::SliderFloat3("Target", glm::value_ptr(m_target), -2.0f, 2.0f);
+    ImGui::SliderFloat("FoV (Degree)", &m_fov, 10.0f, 120.0f);
 }
